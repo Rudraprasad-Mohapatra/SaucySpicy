d3c3feabec9277ae976d2b6c29fdfbfda6aa8efc
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2024-03-01 01:26:30 +0530

    Basic setup done with es-lint import sort

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,12 +10,18 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh','simple-import-sort'],
   rules: {
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
+  "parserOptions": {
+    "sourceType": "module",
+    "ecmaVersion": "latest"
+  }
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,5 @@
+export const Home = () => {
+    return (
+        <div>Home Sweet Home</div>
+    )
+}
